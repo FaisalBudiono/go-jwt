89c876c1a9fdc6324466769394ed2bb21b91ce6f
Faisal Budiono <ucul.project@gmail.com>
2024-08-19 22:01:56 +0700

    feat(endpoint): add sign up endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"FaisalBudiono/go-jwt/internal/app/adapter"
+	"FaisalBudiono/go-jwt/internal/app/core"
+	"FaisalBudiono/go-jwt/internal/app/port"
 	"FaisalBudiono/go-jwt/internal/db"
 	"FaisalBudiono/go-jwt/internal/env"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func init() {
@@ -10,5 +18,56 @@ func init() {
 	db.Init()
 }
 
+type ErrorRes struct {
+	Error string `json:"error"`
+}
+
 func main() {
+	e := echo.New()
+
+	adb := adapter.NewDB(db.DB)
+	hasher := core.NewPwHasher()
+	cauth := core.NewAuth(adb, hasher)
+
+	e.Use(middleware.Logger())
+
+	e.POST("/sign-up", func(c echo.Context) error {
+		type req struct {
+			Name     string `json:"name"`
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+
+		var r req
+		err := c.Bind(&r)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, ErrorRes{err.Error()})
+		}
+
+		u, err := cauth.Reg(port.RegisterInput{
+			Ctx:      c.Request().Context(),
+			Name:     r.Name,
+			Email:    r.Email,
+			Password: r.Password,
+		})
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, ErrorRes{err.Error()})
+		}
+
+		return c.JSON(http.StatusOK, struct {
+			ID        string    `json:"id"`
+			Name      string    `json:"name"`
+			Email     string    `json:"email"`
+			CreatedAt time.Time `json:"createdAt"`
+			UpdatedAt time.Time `json:"updatedAt"`
+		}{
+			ID:        u.ID,
+			Name:      u.Name,
+			Email:     u.Email,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,21 @@ module FaisalBudiono/go-jwt
 go 1.22.5
 
 require (
+	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
 	github.com/pressly/goose/v3 v3.21.1
 	github.com/spf13/viper v1.19.0
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
 
 require (
